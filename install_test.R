d7599e81f89c9c9e317d49e9d136148d008683f2
thisray <gn01697933@gmail.com>
2016-09-10 14:45:38 +0800

    Update install_test.R

--- install_test.R
+++ install_test.R
@@ -11,15 +11,13 @@ message('install ok')
 
 if(grepl('mingw', sessionInfo()$R.version$os)){
 	Sys.setlocale(category = "LC_ALL", locale = "cht")
-	select_language(language="cht_win2")
+	select_language(language="cht_win")
 }else{
 	Sys.setlocale(category = "LC_ALL", locale = "zh_TW.UTF-8")
 	select_language(language="cht_mac")
 }
 
 
-# message("目前預設語言為繁體中文, 如需使用英文介面請輸入指令 : select_language\(language=\"english\"\) ")
-# message("開始課程請輸入 : swirl()")
 # select_language(language="chinese_traditional")
 # select_language(language="english")
 # swirl()
