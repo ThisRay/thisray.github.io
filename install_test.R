4c544b3dd56e5d35dd6cfa401f1469efec91a615
thisray <gn01697933@gmail.com>
2016-09-12 16:55:51 +0800

    Update install_test.R

--- install_test.R
+++ install_test.R
@@ -4,10 +4,12 @@ message('| start install')
 ####
 
 local({
-  try(install.packages('testthat', repos = "https://github.com/hadley/testthat"))
+  
   check_package <- function(pkg) {
     if (!suppressWarnings(suppressMessages(require(pkg, character.only = TRUE)))) {
-      try(install.packages(pkg, repos = "http://wush978.github.io/R"))
+      if(pkg!="testthat"){try(install.packages(pkg, repos = "http://wush978.github.io/R"))
+      }else{try(install.packages('testthat', repos = "https://github.com/hadley/testthat"))}
+      
       try(install.packages(pkg, repos = c("http://cran.ism.ac.jp/", "http://rstudio.org/_packages", "http://cran.rstudio.com"))) 
       # , type="source"
       #install.packages(pkg, repos = "http://wush978.github.io/R")
