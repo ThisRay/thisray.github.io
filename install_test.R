6fa513753b275fa0a43ff433cf2542eead745bff
thisray <gn01697933@gmail.com>
2016-09-13 14:38:53 +0800

    Update install_test.R

--- install_test.R
+++ install_test.R
@@ -6,8 +6,9 @@ message('| start install')
 local({
 	check_package <- function(pkg) { 
 		if(!suppressWarnings(suppressMessages(require(pkg, character.only = TRUE)))) {
-			try(install.packages(pkg, repos = "http://wush978.github.io/R"))
 			try(install.packages(pkg))
+			try(install.packages(pkg, repos = "http://wush978.github.io/R"))
+			
 			# try(install.packages(pkg, repos = c("http://cran.ism.ac.jp/", "http://rstudio.org/_packages", "http://cran.rstudio.com")))
 			# , type="source"
 			# 
